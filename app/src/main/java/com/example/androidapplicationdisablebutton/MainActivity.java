f14a54fb8694ff4cc1e686fec32f4882f23278a1
drsnvs <darshanvijaybhai@gmail.com>
2024-02-04 13:45:23 +0530

    Layout Done

--- app/src/main/java/com/example/androidapplicationdisablebutton/MainActivity.java
+++ app/src/main/java/com/example/androidapplicationdisablebutton/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.androidapplicationdisablebutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText u1,p1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        u1 = (EditText) findViewById(R.id.username);
+        String uname = u1.getText().toString();
+        p1 = (EditText) findViewById(R.id.password);
+        String pass = p1.getText().toString();
+        Button b = (Button) findViewById(R.id.submit);
+        if(u1.equals("")||p1.equals("")){
+            b.setEnabled(false);
+        }else{
+            b.setEnabled(true);
+        }
+
     }
+
 }
