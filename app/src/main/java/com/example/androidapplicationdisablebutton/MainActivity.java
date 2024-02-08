bb975c3dea4b5081e1fc8edd3f6c28aca45ea335
drsnvs <darshanvijaybhai@gmail.com>
2024-02-08 21:20:55 +0530

    Work

--- app/src/main/java/com/example/androidapplicationdisablebutton/MainActivity.java
+++ app/src/main/java/com/example/androidapplicationdisablebutton/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.androidapplicationdisablebutton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText u1,p1;
@@ -17,11 +22,71 @@ public class MainActivity extends AppCompatActivity {
         p1 = (EditText) findViewById(R.id.password);
         String pass = p1.getText().toString();
         Button b = (Button) findViewById(R.id.submit);
-        if(u1.equals("")||p1.equals("")){
-            b.setEnabled(false);
-        }else{
-            b.setEnabled(true);
-        }
+        b.setEnabled(false);
+//        if(u1.equals("")||p1.equals("")){
+//            b.setEnabled(false);
+//        }else{
+//            b.setEnabled(true);
+//        }
+        p1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //Toast.makeText(MainActivity.this, "Before", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(u1.getText().toString().equals("Darshan") && p1.getText().toString().equals("Darshan")){
+                    b.setEnabled(true);
+                }else{
+                    b.setEnabled(false);
+                }
+
+                //Toast.makeText(MainActivity.this, "After", Toast.LENGTH_SHORT).show();
+            }
+        });
+        u1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //Toast.makeText(MainActivity.this, "Before", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(u1.getText().toString().equals("Darshan") && p1.getText().toString().equals("Darshan")){
+                    b.setEnabled(true);
+                }else{
+                    b.setEnabled(false);
+                }
+
+                //Toast.makeText(MainActivity.this, "After", Toast.LENGTH_SHORT).show();
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(u1.getText().toString().equals("Darshan") && p1.getText().toString().equals("Darshan")){
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    intent.putExtra("keyword",u1.getText().toString());
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Login unsuccessfull", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
 }
--- app/src/main/java/com/example/androidapplicationdisablebutton/MainActivity2.java
+++ app/src/main/java/com/example/androidapplicationdisablebutton/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.androidapplicationdisablebutton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView txt = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String s = intent.getStringExtra("keyword");
+        txt.setText("Welcome "+s);
     }
 }
